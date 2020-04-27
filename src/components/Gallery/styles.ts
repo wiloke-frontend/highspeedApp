ad256e920545c95a9fc6706ce12a8d4e5bc9a890
Wiloke <hpvanlong@gmail.com>
2020-04-27 18:04:54 +0700

    fix gallery bg

--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -2,6 +2,9 @@ import { StyleSheet } from 'react-native';
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from 'shared/utils/screen';
 
 export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#111',
+  },
   item: {
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
