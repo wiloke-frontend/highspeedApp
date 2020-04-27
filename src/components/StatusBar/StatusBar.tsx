fc30ddcba24ea9dff2b97d87aebd52c3851b3a63
Wiloke <hpvanlong@gmail.com>
2020-04-27 18:36:17 +0700

    fix statusBar android

--- src/components/StatusBar/StatusBar.tsx
+++ src/components/StatusBar/StatusBar.tsx
@@ -2,11 +2,19 @@ import React, { FC, memo } from 'react';
 import { StatusBar as RNStatusBar, StatusBarProps } from 'react-native';
 import { useSelector } from 'react-redux';
 import { nightModeSelector } from 'containers/ProfileScreen/selectors';
+import { useTheme } from 'shared';
 
-const StatusBar: FC<StatusBarProps> = ({ barStyle, ...rest }) => {
+const StatusBar: FC<StatusBarProps> = ({ barStyle, backgroundColor, ...rest }) => {
   const nightMode = useSelector(nightModeSelector);
+  const { colors } = useTheme();
 
-  return <RNStatusBar {...rest} barStyle={barStyle ? barStyle : nightMode ? 'light-content' : 'dark-content'} />;
+  return (
+    <RNStatusBar
+      {...rest}
+      backgroundColor={!!backgroundColor ? backgroundColor : colors.light}
+      barStyle={!!barStyle ? barStyle : nightMode ? 'light-content' : 'dark-content'}
+    />
+  );
 };
 
 export default memo(StatusBar);
